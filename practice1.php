59bf18ef5d033d348388abf9fbea17806ee2c3ae
egorbenko <efim.gorbenko@gmail.com>
2015-10-24 17:31:21 -0400

    structure

--- practice1.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: efim
- * Date: 22/10/15
- * Time: 11:58 PM
- */
---
+++ task1/test/MainTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: efim
+ * Date: 24/10/15
+ * Time: 4:19 PM
+ */
+
+class MainTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testMain()
+    {
+
+    }
+    
+}
