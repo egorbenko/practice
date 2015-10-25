35d618556e3a4b93f4142e1a79fe4d0ee82261d3
Flavius Aspra <flavius.as@gmail.com>
2015-10-25 06:10:03 +0100

    test that the runner can execute a closure with a fake this object

---
+++ task1/test/IntegrationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class IntegrationTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * This is the first big integration test.
+     *
+     * @test
+     */
+    public function the_runner_can_execute_a_closure_with_a_fake_this_object()
+    {
+        $runner = new \task1\Runner();
+        $strategy = function() {
+            $this->logGreeting("hello world");
+            return $this->getAnswer() === 42;
+        };
+        $evaluation = $runner->execute($strategy);
+
+        $this->assertTrue($evaluation);
+    }
+}
