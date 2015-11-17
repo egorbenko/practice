10e31c21548cc72088f124b59d46bb2eaf2b3154
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-11-17 09:35:01 -0500

    removing old code - starting fresh

--- task1/src/Runner.php
+++ task1/src/Runner.php
@@ -5,10 +5,8 @@ namespace task1\src;
 
 class Runner
 {
-    /**
-     * @param AbstractClass $abstractClass
-     */
-    public function execute(AbstractClass $abstractClass)
+
+    public function execute()
     {
 
     }
