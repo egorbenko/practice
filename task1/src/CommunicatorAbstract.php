670a06b51e6cd5a6e8826d7f9ce12dd3a83717e7
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-11-17 10:34:16 -0500

    adding CommunicatorAbstract class that implements ICommunicate methods to fulfill the contract

---
+++ task1/src/CommunicatorAbstract.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: efim
+ * Date: 17/11/15
+ * Time: 10:11 AM
+ */
+
+namespace task1\src;
+
+
+class CommunicatorAbstract implements ICommunicate
+{
+    public function logGreeting($greeting)
+    {
+
+    }
+
+    public function getAnswer()
+    {
+
+    }
+}
