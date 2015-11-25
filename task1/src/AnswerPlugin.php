5134129774a8e63727928998bbfab51afbadffa7
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-11-24 22:41:48 -0500

    making Greeter and Answer plugins callable

--- task1/src/AnswerPlugin.php
+++ task1/src/AnswerPlugin.php
@@ -5,6 +5,11 @@ namespace task1;
 
 class AnswerPlugin implements BoundObjectPlugin
 {
+    public function __invoke()
+    {
+        $this->getAnswer();
+    }
+
     public function getAnswer()
     {
         return 42;
--- task1/src/GreeterPlugin.php
+++ task1/src/GreeterPlugin.php
@@ -5,6 +5,11 @@ namespace task1;
 
 class GreeterPlugin implements BoundObjectPlugin
 {
+    public function __invoke($greeting)
+    {
+        $this->logGreeting($greeting);
+    }
+
     public function logGreeting($greeting)
     {
         echo $greeting;
