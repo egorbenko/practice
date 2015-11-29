c0f21d34110642486b4ff5bc84c908b2a75d461e
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-11-29 15:05:27 -0500

    adding __invoke method to an interface and changing signature of it's implementors

--- task1/src/GreeterPlugin.php
+++ task1/src/GreeterPlugin.php
@@ -5,7 +5,7 @@ namespace task1;
 
 class GreeterPlugin implements BoundObjectPlugin
 {
-    public function __invoke($greeting)
+    public function __invoke(array $greeting = null)
     {
         $this->logGreeting($greeting);
     }
