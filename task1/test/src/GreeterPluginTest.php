e67c4dbbbbf70be3cc476fe613621c5b172b565a
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-11-30 14:02:10 -0500

    correcting the message to match "hello world" from integration test

--- task1/test/src/GreeterPluginTest.php
+++ task1/test/src/GreeterPluginTest.php
@@ -7,7 +7,7 @@ class GreeterPluginTest extends PHPUnit_Framework_TestCase
      */
     public function log_greeting_echoes_message()
     {
-        $message = 'hello, world!';
+        $message = 'hello world';
         $greeterPlugin = new \task1\GreeterPlugin();
         $greeterPlugin->logGreeting($message);
 
