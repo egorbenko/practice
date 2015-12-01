db3a8f9349b721dabe152bcff0abac30a0b5fb81
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-12-01 11:01:23 -0500

    passing runner's closure to execute to create endless recursion

--- task1/test/integration/IntegrationTest.php
+++ task1/test/integration/IntegrationTest.php
@@ -4,20 +4,16 @@ use task1\Runner;
 
 class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
      * This is the first big integration test.
      *
-     * @test
+     * @tests
      */
     public function the_runner_can_execute_a_closure_with_a_fake_this_object()
     {
         $runner = new Runner();
-        $strategy = function() {
-            $this->logGreeting("hello world");
-            return $this->getAnswer() === 42;
-        };
-        $evaluation = $runner->execute($strategy);
+
+        $evaluation = $runner->execute($runner->getClosure());
 
         $this->assertTrue($evaluation);
     }
