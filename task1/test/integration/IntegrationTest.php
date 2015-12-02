af1a4e9c835e68a9b93bf06fa0edfcb1878414ec
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-12-01 22:04:19 -0500

    Revert "passing runner's closure to execute to create endless recursion"
    This reverts commit db3a8f9349b721dabe152bcff0abac30a0b5fb81.

--- task1/test/integration/IntegrationTest.php
+++ task1/test/integration/IntegrationTest.php
@@ -4,16 +4,20 @@ use task1\Runner;
 
 class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
+
     /**
      * This is the first big integration test.
      *
-     * @tests
+     * @test
      */
     public function the_runner_can_execute_a_closure_with_a_fake_this_object()
     {
         $runner = new Runner();
-
-        $evaluation = $runner->execute($runner->getClosure());
+        $strategy = function() {
+            $this->logGreeting("hello world");
+            return $this->getAnswer() === 42;
+        };
+        $evaluation = $runner->execute($strategy);
 
         $this->assertTrue($evaluation);
     }
