ebc088b3f62d0db753549c576465c2ea1d19ca70
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-12-06 10:56:44 -0500

    removing unnecessary __call method from Runner

--- task1/src/Runner.php
+++ task1/src/Runner.php
@@ -15,15 +15,6 @@ class Runner
         $this->plugins = $this->newDefaultPlugins();
     }
 
-    public function __call($name, $arguments)
-    {
-        if (array_key_exists($name, $this->plugins)) {
-            return $this->plugins[$name]($arguments);
-        }
-
-        throw new \Exception("Plugin: '" . $name . "' not found.");
-    }
-
     private function newDefaultPlugins()
     {
         $plugins  = [
