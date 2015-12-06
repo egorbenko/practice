30429143291143162ee4916c9847f93033b56e16
Efim Gorbenko <efim.gorbenko@gmail.com>
2015-12-06 10:48:20 -0500

    adding RestrictedEnvironment for plugin execution

---
+++ task1/src/RestrictedEnvironment.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace task1;
+
+
+class RestrictedEnvironment
+{
+    /**
+     * @var BoundObjectPlugin[]
+     */
+    private $plugins;
+
+    public function __construct($plugins)
+    {
+        $this->plugins = $plugins;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (array_key_exists($name, $this->plugins)) {
+            return $this->plugins[$name]($arguments);
+        }
+
+        throw new \Exception("Plugin: '" . $name . "' not found.");
+    }
+}
--- task1/src/Runner.php
+++ task1/src/Runner.php
@@ -15,15 +15,6 @@ class Runner
         $this->plugins = $this->newDefaultPlugins();
     }
 
-    public function __call($name, $arguments)
-    {
-        if (array_key_exists($name, $this->plugins)) {
-            return $this->plugins[$name]($arguments);
-        }
-
-        throw new \Exception("Plugin: '" . $name . "' not found.");
-    }
-
     private function newDefaultPlugins()
     {
         $plugins  = [
@@ -36,6 +27,6 @@ class Runner
 
     public function execute(\Closure $closure)
     {
-        return $closure->call($this);
+        return $closure->call(new RestrictedEnvironment($this->plugins));
     }
 }
